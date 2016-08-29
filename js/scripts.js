e730b32bea862b461ced67b26c3dd4f2064a6bd0
Alvaro Flores and Garrett Boggs <alvaro@>
2016-08-29 11:59:07 -0700

    add dialogue code

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,19 @@ function Area(location1, commands)
 }
 
 //Backend
+var names = ["Brotimus", "Skate Bro", "Vape Bro", "Chest Bro", "Squat Bro", "Zombie Bro", "Dragon Bro", "Mortus", "Bro Hobbit Bobby"];
 function Game(){
   this.potion = 100;
   this.turn = 0;
   this.players = [];
 }
 
-function Player(name,type){
+function Player(name, type){
   this.playerName = name;
   this.hp=0;
   this.maxHP=0;
-  this.attack=0;
+  this.attackMin=0;
+  this.attackMax=0;
   this.playerType = type;
 }
 
@@ -27,59 +29,82 @@ Game.prototype.addHealth = function (){
 }
 
 Game.prototype.attack = function(opponent){
-  this.players[0].hp -= this.players[opponent].attack;
-  this.players[opponent].hp -= this.players[0].attack;
+  var inflictedDamage = 0;
+  console.log("Initial Health: " + this.players[0].hp);
+  console.log("Initial Enemy: "+this.players[opponent].hp);
+  var currentAttack = 0;
+  do{
+    currentAttack = (Math.floor(Math.random() * (this.players[0].attackMax - this.players[0].attackMin + 1)) + this.players[0].attackMin);
+    console.log("Attack: "+currentAttack);
+    inflictedDamage += currentAttack; // may axe
+  this.players[opponent].hp -= currentAttack;
+  if(this.players[opponent].hp <= 0){
+    console.log("got here");
+    break;
+  }
+  this.players[0].hp -= (Math.floor(Math.random() * (this.players[opponent].attackMax - this.players[opponent].attackMin + 1)) + this.players[opponent].attackMin);
+  console.log("Your Health: " + this.players[0].hp);
+  console.log("Your Enemy: "+this.players[opponent].hp);
+  }while(this.players[0].hp > 0 ); //&& this.players[opponent].hp > 0
+  if(this.players[0].hp <= 0){
+    console.log("Game Over Your enemy had " + this.players[opponent].hp + " HP left");
+  }
+  else {
+      console.log("Success");
+      this.players[0].hp += inflictedDamage; // may axe
+    }
+}
 
+Game.prototype.setUpAllPlayers = function(){
+  for (var i = 0; i < names.length; i++) {
+    var myPlayer = new Player(names[i],i);
+    myPlayer.settingPlayer();
+    this.players.push(myPlayer);
+  }
 }
 
 Player.prototype.settingPlayer = function(){
-  if(this.playerType ==='player'){
-    this.hp=100;
-    this.maxHP=200;
-    this.attack=10;
-  }else if(this.playerType === 'easy'){
+  if(this.playerType === 0){
+    this.hp = 100;
+    this.maxHP = 200;
+    this.attackMin = 8;
+    this.attackMax = 12;
+  }else if(this.playerType === 1 || this.playerType ===  2){
     this.hp = Math.floor(Math.random() * (25 - 15 + 1)) + 15;
-    this.hp = Math.floor(Math.random() * (100 - 90 + 1)) + 90;
-    this.attack = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-  }else if(this.playerType === 'medium'){
+    this.attackMin = 1;
+    this.attackMax = 10;
+  }else if(this.playerType === 3 || this.playerType ===  4){
     this.hp = Math.floor(Math.random() * (120 - 75 + 1)) + 75;
-    this.maxHP = Math.floor(Math.random() * (100 - 90 + 1)) + 90;
-    this.attack = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
-  }else {
+    this.attackMin = 5;
+    this.attackMax = 15;
+  }else if(this.playerType === 5 || this.playerType ===  6){
     this.hp = Math.floor(Math.random() * (200 - 100 + 1)) + 100;
-    this.maxHP = Math.floor(Math.random() * (100 - 90 + 1)) + 90;
-    this.attack = Math.floor(Math.random() * (30 - 10 + 1)) + 10;
+    this.attackMin = 10;
+    this.attackMax = 30;
+  }else if(this.playerType === 7){
+    this.hp = Math.floor(Math.random() * (200 - 100 + 1)) + 100;
+    this.attackMin = 10;
+    this.attackMax = 30;
+  }else{ // type 8
+    this.hp = Math.floor(Math.random() * (200 - 100 + 1)) + 100;
+    this.attackMin = 10;
+    this.attackMax = 30;
   }
 }
 
 
 var theGame = new Game();
+theGame.setUpAllPlayers();
 
-var myPlayer = new Player("Andrew","player"); //100,200,10
 
-var myEnemy = new Player("Enemy1","easy"); // 50 50 5
-var myEnemy2 = new Player("Enemy2","medium");
-var myEnemy3 = new Player("Enemy3","hard");
-myPlayer.settingPlayer();
-myEnemy.settingPlayer();
-myEnemy2.settingPlayer();
-myEnemy3.settingPlayer();
-
-console.log(myPlayer);
-console.log(myEnemy);
-console.log(myEnemy2);
-console.log(myEnemy3);
-
-theGame.players.push(myPlayer);
-theGame.players.push(myEnemy);
-theGame.players.push(myEnemy2);
-theGame.players.push(myEnemy3);
-theGame.attack(theGame.turn + 1);
-theGame.attack(theGame.turn + 2);
 theGame.attack(theGame.turn + 3);
+// theGame.attack(theGame.turn + 3);
+// theGame.attack(theGame.turn + 6);
+//
+// theGame.attack(theGame.turn + 7);
+// theGame.attack(theGame.turn + 8);
 
-
-
+console.log(theGame.players);
 
 $(document).ready(function() {
   var counter = 0;
